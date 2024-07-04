a63a61de3db1e173c5248abe06872b6994e6ab89
Majahar Kazi <majahar1152003@gmail.com>
2024-07-04 23:56:19 +0530

    "Made changes into nodejs app configurations"

--- app2.js
+++ app2.js
@@ -1,14 +1,7 @@
-const http = require('http');
+var http = require('http');
 
-const hostname = '0.0.0.0';
-const port = 80;
-
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello, World!\n');
-});
-
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
+//create a server object:
+http.createServer(function (req, res) {
+  res.write('Hello World'); //write a response to the client
+  res.end();    //end the response
+}).listen(80);  //the server object listens on port 80
