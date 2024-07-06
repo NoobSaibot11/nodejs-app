4a68dbb954897caf29c5a70f66008a21335aeeed
Majahar Kazi <majahar1152003@gmail.com>
2024-07-06 18:43:59 +0530

    removed testing app

--- app.js
+++
@@ -1,7 +0,0 @@
-var http = require('http');
-
-//create a server object:
-http.createServer(function (req, res) {
-  res.write('Hello World from app 1'); //write a response to the client
-  res.end();    //end the response
-}).listen(3000);  //the server object listens on port 80
